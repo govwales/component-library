093a842370c0b8dad45b2cf8b6c7134336f146cb
Adam Morgan <adam.morgan013@gov.wales>
2024-11-11 13:35:20 +0000

    Allow anchor attributes to be passed into footer links

--- src/components/SiteFooter/SiteFooter.stories.tsx
+++ src/components/SiteFooter/SiteFooter.stories.tsx
@@ -1,11 +1,39 @@
-import React from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 
 // Import React component.
 import SiteFooter from './SiteFooter';
 
-// Menu Example Data.
-import { PrimaryMenuData, SecondaryMenuData } from './DataExample';
+// This data is used as an example.
+export const PrimaryMenuData = [
+  {
+    children: 'Primary Link 1',
+    href: 'https://gov.wales',
+    ['aria-label']: 'Test aria label',
+  },
+  {
+    children: 'Primary Link 2',
+    href: 'https://gov.wales',
+  },
+  {
+    children: 'Primary Link 3',
+    href: 'https://gov.wales',
+  },
+];
+
+export const SecondaryMenuData = [
+  {
+    children: 'Secondary Link 1',
+    href: 'https://gov.wales',
+  },
+  {
+    children: 'Secondary Link 2',
+    href: 'https://gov.wales',
+  },
+  {
+    children: 'Secondary Link 3',
+    href: 'https://gov.wales',
+  },
+];
 
 // Storybook Meta.
 const meta: Meta<typeof SiteFooter> = {
